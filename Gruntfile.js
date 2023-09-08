48306399824e2cbfff9a884a863789a493010ecf
Timo Tijhof <krinkle@fastmail.com>
2023-09-08 21:35:58 +0100

    Build: Remove unused build-member-list task for corporate-members.json
    Follows-up 2018 commit 695da853eb. I'll remove the content filter
    plugin from the jquery-wp-content repo in a separate commit after
    this.

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,63 +18,6 @@ grunt.initConfig({
 	})()
 });
 
-grunt.registerTask( "build-member-list", function() {
-	var https = require( "https" ),
-		done = this.async(),
-		path = grunt.config( "wordpress.dir" ) + "/resources/corporate-members.json";
-
-	function getMembers( level, callback ) {
-		var request = https.request({
-			host: "raw.githubusercontent.com",
-			path: "/jquery/jquery.org/master/data/members/" + level + ".json"
-		}, function ( response ) {
-			var json = "";
-			response.setEncoding( "utf8" );
-			response.on( "data", function( chunk ) {
-				json += chunk;
-			});
-			response.on( "end", function() {
-				try {
-					json = JSON.parse( json );
-				} catch( error ) {
-					return callback( error );
-				}
-
-				callback( null, json );
-			});
-		});
-
-		request.end();
-	}
-
-	async.parallel({
-		diamond: getMembers.bind( null, "diamond" ),
-		platinum: getMembers.bind( null, "platinum" ),
-		gold: getMembers.bind( null, "gold" )
-	}, function( error, members ) {
-		if ( error ) {
-			return done( error );
-		}
-
-		var corporateMembers = members.diamond
-			.concat( members.platinum )
-			.concat( members.gold )
-			.filter(function( member ) {
-
-				// modern.IE has a special membership which doesn't get logo rotation
-				if ( member.name === "modern.IE" ) {
-					return false;
-				}
-
-				return true;
-			});
-
-		corporateMembers = JSON.stringify( corporateMembers );
-		grunt.file.write( path, corporateMembers );
-		done();
-	});
-});
-
-grunt.registerTask( "build", [ "build-posts", "build-resources", "build-member-list" ] );
+grunt.registerTask( "build", [ "build-posts", "build-resources" ] );
 
 };
